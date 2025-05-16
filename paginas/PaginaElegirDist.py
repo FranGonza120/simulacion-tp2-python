bbc45b040dee6f17715019036733539596f9e80b
FranGonza120 <franco20047@gmail.com>
2025-05-16 07:39:06 -0300

    Se quito label de Advertencia de Dist Normal

--- paginas/PaginaElegirDist.py
+++ paginas/PaginaElegirDist.py
@@ -22,32 +22,28 @@ class PaginaElegirDist(PaginaBase):
         self.set_boton_extra_texto("Continuar")
         self.conectar_boton_extra(self.enviar_datos)
 
-        label_input_dist = QLabel("Seleccione una distribucion con la que quiera generar los valores: ")
+        label_input_dist = QLabel(
+            "Seleccione una distribucion con la que quiera generar los valores: ")
         label_input_dist.setWordWrap(True)
         self.agregar_widget(label_input_dist)
         self.agregar_widget(self.combo)
         self.agregar_widget(QLabel(" "))
-        
-        label_intervalos = QLabel("Seleccione la cantidad de intervalos para el histograma:")
+
+        label_intervalos = QLabel(
+            "Seleccione la cantidad de intervalos para el histograma:")
         label_intervalos.setWordWrap(True)
         self.agregar_widget(label_intervalos)
         self.agregar_widget(self.intervalos_combo)
         self.agregar_widget(QLabel(" "))
-        
-        label_input_val = QLabel("Ingrese la cantidad de valores que desea generar (1 a 1.000.000):")
+
+        label_input_val = QLabel(
+            "Ingrese la cantidad de valores que desea generar (1 a 1.000.000):")
         label_input_val.setWordWrap(True)
         self.agregar_widget(label_input_val)
         self.agregar_widget(self.spin)
-        
-        label_aviso = QLabel(
-            "(Para la distribución Normal elija un número par o será elegido el próximo número par a su valor ingresado)"
-        )
-        label_aviso.setWordWrap(True)
-        self.agregar_widget(label_aviso)
 
     def enviar_datos(self):
         dist = self.combo.currentText()
         cantidad = self.spin.value()
         intervalos = int(self.intervalos_combo.currentText())
         self.callback(dist, cantidad, intervalos)
-
